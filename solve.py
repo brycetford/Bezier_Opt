efdb296324a49f8d2e20beadd3885263019036d5
Bryce <brycetford@gmail.com>
2021-10-17 15:26:52 -0400

    Added basic gradient computation

--- solve.py
+++ solve.py
@@ -5,37 +5,54 @@ import matplotlib.pyplot as plt
 
 def loading_function():
     Cov = np.mat('2 0; 0 5')
-    Loc = np.mat('2; 2')
-    Rot = math.pi / 4
+    Loc = np.mat('1; 1')
+    Rot = 0
     return fun.Gaussian2d(Cov, Loc, Rot)
 
 def eval_load(path, load_fun, n):
 
-    s_t = []
-    for i in range(n):
-        t = i/n
-        s_t.append(path.eval(t))
-
-    s = np.array([s_t]).T
-    plt.scatter(s[:, 0], s[:, 1])
-    plt.show()
-
     load = 0
+    li = load_fun.eval(path.eval(0))
+
     for i in range(n-1):
-        u = load_fun.eval(s_t[i])
-        h = load_fun.eval(s_t[i+1])
-        d_l = (u+h)/2
-        load += d_l
+        dt = (i+1)/n
+        lf = load_fun.eval(path.eval(dt))
+        load += dt*(li + lf)/2     # Center
+        li = lf
 
     return load
+
+def gradient(load_fun, curr_load, params, n):
+
+    h = 0.1     # How far to 'nudge' each parameter in gradient computation
+    size = params.shape
+
+    d_dx = params + np.array([0, h, 0, 0, 0, 0]).reshape(size)
+    d_dy = params + np.array([0, 0, 0, 0, h, 0]).reshape(size)
+
+    d_dBx = fun.Bezier(2, 3, d_dx)
+    d_dBy = fun.Bezier(2, 3, d_dy)
+
+    dL_dBx = (eval_load(d_dBx, load_fun, n) - curr_load) / h
+    dL_dBy = (eval_load(d_dBy, load_fun, n) - curr_load) / h
+
+    return np.array([0, dL_dBx, 0, 0, dL_dBy, 0]).reshape(size)
 
 
 if __name__ == "__main__":
     # Initial guess for path
-    Params = np.mat('[0 0; 0 2; 2 -1; 3 3]').T
-    B = fun.Bezier(2, 4, Params)
+    Params = np.mat('[0 0; 1 1; 2 2]').T    # Initial control points fot the Bezier curve
+    B = fun.Bezier(2, 3, Params)
 
+    # Loading function
     Load = loading_function()
 
-    curr_load = eval_load(B, Load, 20)
-    print(curr_load)
+    alpha = 0.05    # Gradient descent rate
+    n = 20  # Iterations for numerical computations
+
+    for i in range(5):
+        Curr_Load = eval_load(B, Load, n)
+        Params = Params -alpha*gradient(Load, Curr_Load, Params, n)
+        B = fun.Bezier(2, 3, Params)
+
+    print(Params)
