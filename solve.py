20ebf76c5419f74f611c9fc3cdf30f2f3376d3cb
Bryce <brycetford@gmail.com>
2021-10-17 21:40:51 -0400

    Added parameter space cost visualization for 2nd order curve.

--- solve.py
+++ solve.py
@@ -1,30 +1,43 @@
 import numpy as np
 import math
+import time
 import fun
 import matplotlib.pyplot as plt
+from matplotlib import pyplot
+from mpl_toolkits import mplot3d
 
 def loading_function():
     Cov = np.mat('2 0; 0 5')
-    Loc = np.mat('1; 1')
+    Loc = np.mat('0; 1')
     Rot = 0
     return fun.Gaussian2d(Cov, Loc, Rot)
 
-def eval_load(path, load_fun, n):
 
+def plot_curve(bezier, n):
+    s_t = []
+    for i in range(n):
+        t = i / n
+        s_t.append(bezier.eval(t))
+    s_xy = np.array([s_t]).T
+    plt.scatter(s_xy[:, 0], s_xy[:, 1])
+    plt.show()
+
+
+def eval_load(path, load_fun, n):
     load = 0
     li = load_fun.eval(path.eval(0))
 
-    for i in range(n-1):
-        dt = (i+1)/n
+    for i in range(n - 1):
+        dt = (i + 1) / n
         lf = load_fun.eval(path.eval(dt))
-        load += dt*(li + lf)/2     # Center
+        load += dt * (li + lf) / 2  # Center
         li = lf
 
     return load
 
-def gradient(load_fun, curr_load, params, n):
 
-    h = 0.1     # How far to 'nudge' each parameter in gradient computation
+def gradient(load_fun, curr_load, params, n):
+    h = 0.1  # How far to 'nudge' each parameter in gradient computation
     size = params.shape
 
     d_dx = params + np.array([0, h, 0, 0, 0, 0]).reshape(size)
@@ -39,20 +52,45 @@ def gradient(load_fun, curr_load, params, n):
     return np.array([0, dL_dBx, 0, 0, dL_dBy, 0]).reshape(size)
 
 
+def visualize_cost(load_fun, n):
+    r_min, r_max = 0, 2
+    xaxis = np.arange(r_min, r_max, 0.1)
+    yaxis = np.arange(r_min, r_max, 0.1)
+    x, y = np.meshgrid(xaxis, yaxis)
+    results = load_wrapper(x, y, load_fun)
+    figure = pyplot.figure()
+    axis = figure.gca(projection='3d')
+    axis.plot_surface(x, y, results, cmap='jet')
+    pyplot.show()
+
+
+def load_wrapper(x, y, load_fun, n):
+    results = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            Params = np.mat([0, x[i, j], 2, 0, y[i, j], 2]).reshape(2, 3)
+            B = fun.Bezier(2, 3, Params)
+            results[i, j] = eval_load(B, load_fun, n)
+
+    return results
+
+
 if __name__ == "__main__":
     # Initial guess for path
-    Params = np.mat('[0 0; 1 1; 2 2]').T    # Initial control points fot the Bezier curve
+    Params = np.mat('[0 0; 1 1; 2 2]').T  # Initial control points fot the Bezier curve
     B = fun.Bezier(2, 3, Params)
 
     # Loading function
     Load = loading_function()
+    visualize_cost(Load)
 
-    alpha = 0.05    # Gradient descent rate
+    alpha = 0.05  # Gradient descent rate
     n = 20  # Iterations for numerical computations
 
-    for i in range(5):
+    for i in range(n):
         Curr_Load = eval_load(B, Load, n)
-        Params = Params -alpha*gradient(Load, Curr_Load, Params, n)
+        Params = Params - alpha * gradient(Load, Curr_Load, Params, n)
         B = fun.Bezier(2, 3, Params)
+        #plot_curve(B, n)
 
     print(Params)
